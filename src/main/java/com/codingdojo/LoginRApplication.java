d460953c6a0e5c055b9593b238bd960d8b6b4b3e
Andres Rodriguez <kflex25@MacBook-Pro-de-Andres.local>
2021-11-08 15:54:24 -0600

    LogAndReg

---
+++ src/main/java/com/codingdojo/LoginRApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRApplication.class, args);
+	}
+
+}
